8c110ce61f35e49447300ff256e636cd6efcd89d
mappingvermont <charlie.hofmann@gmail.com>
2016-08-08 06:06:31 -0400

    clean up PROD environment

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
 from flask import Flask, request, jsonify, Response
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.api import status
-#from flask_cors import CORS, cross_origin
 import os
 import json
 from hashids import Hashids
 import sys
 import logging
 
-#logging.getLogger('flask_cors').level = logging.DEBUG
 
 app = Flask(__name__)
-#cors = CORS(app, resources={r"/collection/*": {"origins": "*"}})
-#cors = CORS(app, resources={r"/collection/*": {"origins": "http://xfade.audio/"}})
-#app.config.from_object(os.environ['CROSSFADE_APP_SETTINGS'])
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['CROSSFADE_DATABASE_URL']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///crossfade'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#app.config['CORS_HEADERS'] = 'Content-Type'
 
 db = SQLAlchemy(app)
 hashids = Hashids(min_length=8)
@@ -43,30 +36,15 @@ def new():
 
         db.session.commit()
 
-        #output = jsonify({'collection_id': hashid})
         return  jsonify({'collection_id': hashid})
 
     else:
-
+        # OPTIONS request
         resp = Response(status=200)
         resp.headers['Allow'] = 'POST'
 
         return resp
 
-
-#        content = {'Allow': 'POST'}
-#        return jsonify({'ok': 'ok'})
-#        return (content, status.HTTP_200_OK)
-#        return status.HTTP_200_OK
-#        return {'Allow' : 'POST' }, status.HTTP_200_OK
-#        return {'Allow' : 'POST' } , 200
-#        return  200
-
-#        output = jsonify({'options': 'ok'})
- #       output = jsonify({'options': 'ok'})
-
-        #return ('', 204)
-  #  return output
 
 @app.route('/collection/<hash_id>', methods=['GET'])
 def get_collection(hash_id):
